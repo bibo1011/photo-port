befbb79dee5983c8488b23c11246d7290bfd5f2f
Blue Abad <bibo1011@gmail.com>
2020-12-11 22:25:15 -0800

    modal test not done

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render, cleanup,  fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Modal from '../'
+
+afterEach(cleanup)
+
+//test not done
